4ad2a220e000338ab55dc6673cc67a03a604e617
JossBa <jossbattiston@gmail.com>
2025-02-09 21:31:16 +0100

    CH2 L9 - Add sprite groups

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ from player import Player
 def main():
     pygame.init()
     time_clock = pygame.time.Clock()
-    dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, )
-    player.draw(screen)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, )
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
+
         screen.fill((0,0,0))
-        player.draw(screen)
+        for p in drawable:
+            p.draw(screen)
 
         # limit the framerate to 60 FPS
         pygame.display.flip()
