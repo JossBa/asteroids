b7ae56820a87c404d9cd0934645078adc6bacdbd
JossBa <jossbattiston@gmail.com>
2025-03-02 12:01:17 +0100

    added split on collision

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from constants import ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -10,3 +12,16 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        neg_rotated_velocity = self.velocity.rotate(-random_angle)
+        pos_rotated_velocity = self.velocity.rotate(random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_1.velocity = neg_rotated_velocity * 1.3
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2.velocity = pos_rotated_velocity * 1.3
